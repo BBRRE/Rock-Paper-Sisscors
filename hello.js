3972f0da4871ae7dbd7e0a5a054070481cb7c7bb
BBRRE <Bilaalbarre@hotmail.com>
2023-08-06 03:18:48 +0100

    cleaned up code and got the round system to work

--- hello.js
+++ hello.js
@@ -1,23 +1,45 @@
-let answer = prompt("Rock Paper or Scissors").toLowerCase()
+
 
 function getComputerChoice(){
     arr = ["rock","paper","scissors"]
     return arr.at(Math.floor(Math.random() * 3))
 }
- 
-function playRound(playerSelection,computerSelection){
+
+function up(name){
+return name.charAt(0).toUpperCase() + name.slice(1)
+}
+
+ let w = 0
+ let l = 0
+ let d = 0
+
+for (i = 1; i < 6; i++){
+    let answer = prompt("Rock Paper or Scissors").toLowerCase()
+
+    function playRound(playerSelection,computerSelection){
     if (playerSelection === computerSelection){
+        d += 1
         console.log("Draw!")
     }
     else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "rock"  ){
-        console.log("You win! " + playerSelection + " beats " + computerSelection)
+        w += 1
+        console.log("You win! " + up(playerSelection) + " beats " + up(computerSelection))
     }
     else{
-        console.log("You loose! " + computerSelection + " beats " + playerSelection)
+        l += 1
+        console.log("You loose! " + up(computerSelection) + " beats " + up(playerSelection))
     }
 }
-let comp = getComputerChoice()
 
-console.log("Your answer " + answer)
-console.log("Computer answer " + comp)
+let comp = getComputerChoice()
+console.log("Round: " + i)
+console.log("Your answer: " + up(answer))
+console.log("Computer answer: " + up(comp))
 console.log(playRound(answer,comp))
+
+}
+
+console.log("results: ")
+console.log("Player Wins: " + w)
+console.log("Computer Wins: " + l)
+console.log("Draws: " + d)
