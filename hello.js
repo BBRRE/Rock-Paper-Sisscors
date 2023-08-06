7a21c59d3c1010f4cd399fd0cd2e7e64114024a1
BBRRE <Bilaalbarre@hotmail.com>
2023-08-06 10:01:57 +0100

    Jai fini
    Cba to do the limit after 5 wins/loses
    more fun without

--- hello.js
+++ hello.js
@@ -1,23 +1,43 @@
-let answer = prompt("Rock Paper or Scissors").toLowerCase()
 
 function getComputerChoice(){
     arr = ["rock","paper","scissors"]
     return arr.at(Math.floor(Math.random() * 3))
 }
- 
+
+function up(name){
+return name.charAt(0).toUpperCase() + name.slice(1)
+}
+
 function playRound(playerSelection,computerSelection){
     if (playerSelection === computerSelection){
-        console.log("Draw!")
+        d += 1
+        result.innerHTML = "Result: " +  "Draw!"
     }
     else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "rock"  ){
-        console.log("You win! " + playerSelection + " beats " + computerSelection)
+        w += 1
+        result.innerHTML = "Result: " +  "You win! " + playerSelection + " beats " + computerSelection
     }
     else{
-        console.log("You loose! " + computerSelection + " beats " + playerSelection)
+        l += 1
+        result.innerHTML = "Result: " + "You loose! " + computerSelection + " beats " + playerSelection
     }
+    document.querySelector(".draw").innerHTML = "Draw: " + d;
+    document.querySelector(".player").innerHTML = "Player: " + w;
+    document.querySelector(".computer").innerHTML = "Computer: " + l;
+    document.querySelector(".round").innerHTML = "Round: " + ( d + w + l );
 }
-let comp = getComputerChoice()
+const result = document.querySelector(".result")
 
-console.log("Your answer " + answer)
-console.log("Computer answer " + comp)
-console.log(playRound(answer,comp))
+
+const buttons = document.querySelectorAll("button")
+let player;
+let w = 0
+let d = 0
+let l = 0
+buttons.forEach((button) =>{
+    button.addEventListener("click", () => {
+    playRound(button.id,getComputerChoice())
+    })
+})
+
+console.log(player)
